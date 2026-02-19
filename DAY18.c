ddc5d0bf2639cde89b4f73c8699c0eac29616228
siya09568-ux <siya09568@gmail.com>
2026-02-19 08:11:04 +0530

    Implement array rotation in DAY18.c

---
+++ DAY18.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[n];
+
+    for(int i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
+
+    for(int i = k; i < n; i++) {
+        temp[i] = arr[i - k];
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", temp[i]);
+    }
+
+    return 0;
+}
