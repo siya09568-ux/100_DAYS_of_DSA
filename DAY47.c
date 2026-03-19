1e3a2fe334bf2c8ab892a1d56080c4287c9abb28
siya09568-ux <siya09568@gmail.com>
2026-03-19 07:09:12 +0530

    Create DAY47.c

---
+++ DAY47.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+// Create node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order
+Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+// Find height
+int height(Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    Node* root = buildTree(arr, n, 0);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
