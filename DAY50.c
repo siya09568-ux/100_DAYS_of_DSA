fcc477e5fc3d8a1ba19f7095bd05643acbb0b08d
siya09568-ux <siya09568@gmail.com>
2026-03-22 22:20:56 +0530

    Create DAY50.c

---
+++ DAY50.c
@@ -0,0 +1,64 @@
+//BST Search
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+// Create node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+Node* insert(Node* root, int data) {
+    if (root == NULL)
+        return createNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+// Search in BST
+int search(Node* root, int key) {
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n, x, key;
+    scanf("%d", &n);
+
+    Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
+    return 0;
+}
