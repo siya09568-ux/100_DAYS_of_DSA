046f381856e1b16242ae0a948eac8ebfb03fda8d
siya09568-ux <siya09568@gmail.com>
2026-03-22 22:16:40 +0530

    Create DAY48.c

---
+++ DAY48.c
@@ -0,0 +1,58 @@
+//Count Leaf Nodes
+//Implement the solution for this problem.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+// Create node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order
+Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+// Count leaf nodes
+int countLeaves(Node* root) {
+    if (root == NULL)
+        return 0;
+
+    // If leaf node
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    Node* root = buildTree(arr, n, 0);
+
+    printf("%d\n", countLeaves(root));
+
+    return 0;
+}
